3f5787184ebc84971ee0ee37c55091ba33d00479
paolo <molonepa@tcd.ie>
2020-07-09 13:57:00 +0100

    implement swapchain creation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <optional>
 #include <set>
+#include <algorithm>
 
 const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 const std::vector<const char*> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
@@ -72,6 +73,8 @@ class VulkanTriangleApplication {
 
 		VkDebugUtilsMessengerEXT debugMessenger;
 
+		VkSwapchainKHR swapchain;
+
 		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 		VkDevice logicalDevice;
 
@@ -96,6 +99,7 @@ class VulkanTriangleApplication {
 			createSurface();
 			choosePhysicalDevice();
 			createLogicalDevice();
+			createSwapChain();
 		}
 
 		void createInstance() {
@@ -440,6 +444,96 @@ class VulkanTriangleApplication {
 			if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
 				throw std::runtime_error("ERROR: Failed to create window surface");
 			}
+		}
+
+		VkSurfaceFormatKHR chooseSwapchainSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+			// check available formats for one that supports SRGB
+			for (const auto& availableFormat : availableFormats) {
+				if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+					return availableFormat;
+				}
+			}
+
+			return availableFormats[0]; // return first available format if none support SRGB
+		}
+
+		VkPresentModeKHR chooseSwapchainPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+			// check available present modes for one that supports Mailbox, which can be used to implement triple buffering
+			for (const auto& availablePresentMode : availablePresentModes) {
+				if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+					return availablePresentMode;
+				}
+			}
+
+			return VK_PRESENT_MODE_FIFO_KHR; // use FIFO if Mailbox unavailable
+		}
+
+		VkExtent2D chooseSwapchainExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+			if (capabilities.currentExtent.width != UINT32_MAX) {
+				return capabilities.currentExtent;
+			}
+			else {
+				VkExtent2D actualExtent = {WIDTH, HEIGHT};
+
+				// clamp extent width and height to values supported by surface
+				actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+				actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+				return actualExtent;
+			}
+		}
+
+		void createSwapChain() {
+			SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
+
+			VkSurfaceFormatKHR surfaceFormat = chooseSwapchainSurfaceFormat(swapChainSupport.formats);
+			VkPresentModeKHR presentMode = chooseSwapchainPresentMode(swapChainSupport.presentModes);
+			VkExtent2D extent = chooseSwapchainExtent(swapChainSupport.capabilities);
+
+			// set swapchain image count to supported minimum + 1 to avoid stalling and ensure it doesn't exceed maximum
+			uint32_t imageCount = swapChainSupport.capabilities.minImageCount +1;
+
+			if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
+				imageCount = swapChainSupport.capabilities.maxImageCount;
+			}
+
+			// populate swapchain creation struct with specified values
+			VkSwapchainCreateInfoKHR createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+			createInfo.surface = surface;
+
+			createInfo.minImageCount = imageCount;
+			createInfo.imageFormat = surfaceFormat.format;
+			createInfo.imageColorSpace = surfaceFormat.colorSpace;
+			createInfo.imageExtent = extent;
+			createInfo.imageArrayLayers = 1; // always 1 unless developing stereoscopic 3D application
+			createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+			QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+			uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+			if (indices.graphicsFamily != indices.presentFamily) {
+				// images need not be explicitly transferred between queue families
+				createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+				createInfo.queueFamilyIndexCount = 2;
+				createInfo.pQueueFamilyIndices = queueFamilyIndices;
+			}
+			else {
+				// images are owned by one queue family at a time and must be transferred explicitly
+				createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+				createInfo.queueFamilyIndexCount = 0; // optional
+				createInfo.pQueueFamilyIndices = nullptr; // optional
+			}
+
+			// transforms (e.g. rotation, flipping) can be applied to images in the swapchain, currentTransform specifies no transform
+			createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+
+			createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR; // ignore alpha channel
+
+			createInfo.presentMode = presentMode;
+
+			createInfo.clipped = VK_TRUE;
+			createInfo.oldSwapchain = VK_NULL_HANDLE;
 		}
 
 		void mainLoop() {
